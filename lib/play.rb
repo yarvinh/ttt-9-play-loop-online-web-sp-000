2a028ab917ddb1173cb433eee0128b1570368718
binding-archive-7591 <yarhumher@hotmail.com>
2020-02-10 16:03:51 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -36,3 +36,20 @@ def turn(board)
 end
 
 # Define your play method below
+def play(board)
+  # turn(board)
+  # gets.strip
+ # turn(board)
+ # gets.strip
+
+  board = 1
+  until board == 9
+    turn(board)
+    board += 1
+
+
+
+
+
+  end
+end
